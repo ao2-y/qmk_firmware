9e9b66ba49819ef80443984cd48429e71d910207
ao2-y <blue2.y.developer@gmail.com>
2020-03-08 02:45:23 +0900

    wip

--- keyboards/manta60/keymaps/ao2keymap/keymap.c
+++ keyboards/manta60/keymaps/ao2keymap/keymap.c
@@ -33,15 +33,15 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
   //,---------------------------------------------------------------------.,---------------------------------------------------------------------.
-        KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,  KC_EQL,    KC_PIPE,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_GRV,
+        KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,   KC_UP,    KC_DOWN,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,  KC_GRV,
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
-        KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T, KC_COMM,    KC_DOT,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_MINS,
+        KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T, KC_LEFT,    KC_RGHT,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  KC_MINS,
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
-       KC_LALT,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G, KC_LBRC,    KC_RBRC,     KC_H,     KC_J,     KC_K,     KC_L,    KC_UP,  KC_QUOT,
+       KC_LALT,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G, KC_LBRC,    KC_RBRC,     KC_H,     KC_J,     KC_K,     KC_L,  KC_SCLN,  KC_QUOT,
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
-       KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,  KC_SLSH,                          KC_B,     KC_N,     KC_M,  KC_LEFT,  KC_DOWN,  KC_RGHT,
+       KC_LSFT,     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                          KC_N,     KC_M,  KC_COMM,   KC_DOT,  KC_SLSH,  KC_RSFT,
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
-         LOWER,  XXXXXXX,  KC_LGUI, KC_LCTRL,   KC_SPC,  KC_SPC,  KC_SPC,    KC_BSPC,   KC_ENT,   KC_ENT, KC_BSPC,  KC_RGUI,  XXXXXXX,   KC_RSFT
+         LOWER,  XXXXXXX,  KC_LGUI, KC_LCTRL,   KC_SPC,  KC_SPC,  KC_SPC,    KC_BSPC,   KC_ENT,   KC_ENT, KC_BSPC,  KC_RGUI,    XXXXXXX,   RAISE
   //`---------+----------/\-------+---------+---------+---------+---------/\---------+---------+---------+---------+---------/\--------+----------'
   ),
 
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,---------------------------------------------------------------------.,---------------------------------------------------------------------.
        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
-       _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+    C(KC_LEFT),  XXXXXXX,  KC_UP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,C(KC_RGHT),
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
-       _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+       _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
-       _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
+       _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                       KC_PLUS,  KC_MINS,  KC_EQL,  KC_LBRC,  KC_RBRC, KC_BSLS,
   //|---------+---------+---------+---------+---------+---------+---------|\---------+---------+---------+---------+---------+---------+---------|
          LOWER,  XXXXXXX,  _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  XXXXXXX,   RAISE
   //`---------+----------/\-------+---------+---------+---------+---------/\---------+---------+---------+---------+---------/\--------+----------'
